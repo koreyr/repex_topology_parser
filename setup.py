5abe288b271361b7333f0b8d8f9e42f8a9ef061a
Korey Reid <r.korey@gmail.com>
2025-06-04 15:23:26 -0400

    version 0.1

--- setup.py
+++ setup.py
@@ -22,5 +22,9 @@ setuptools.setup(
     ],
     package_dir = {"": "src"},
     packages = setuptools.find_packages(where="src"),
-    python_requires = ">=3.6"
+    python_requires = ">=3.6",
+    install_requires=[
+        'pandas >=2.2',
+        'numpy <2',
+    ],
 )
